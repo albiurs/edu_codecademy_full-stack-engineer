34e4f6d60006519036c3b80ec659226bf6c80d4a
Urs Albisser <u.albisser@gmail.com>
2020-10-29 08:02:59 +0100

    number-guesser web-based project app added

--- src/03_learn_javascript/05_scope/project02_l01-05_number_guesser_challenge_project/script.js
+++ src/03_learn_javascript/05_scope/project02_l01-05_number_guesser_challenge_project/script.js
@@ -2,13 +2,14 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
-
+// == functions ==
 const generateTarget = () => Math.floor(Math.random() * 10);
 // console.log(generateTarget());
 
-const compareGuess = (humanGuess, computerGuess, secretNumber) => {
 
+const compareGuesses = (humanGuess, computerGuess, secretNumber) => {
+
+    console.log(secretNumber);
     let humanDistance = Math.abs(humanGuess - secretNumber);
     console.log(humanDistance);
     let computerDistance = Math.abs(computerGuess - secretNumber);
@@ -16,4 +17,32 @@ const compareGuess = (humanGuess, computerGuess, secretNumber) => {
 
     return humanDistance < computerDistance || humanDistance === computerDistance;
 }
+// console.log(compareGuess(3,4,5));
+
+
+const updateScore = winner => winner === 'human' ? humanScore++ :
+    winner === 'computer' ? computerScore++ :
+        '';
+
+// const updateScore = winner => {
+//     if(winner === 'human' ) {
+//         humanScore++;
+//     } else if (winner === 'computer') {
+//         computerScore++;
+//     }
+// };
+// console.log(updateScore('human'));
+// console.log(updateScore('human'));
+// console.log(updateScore('human'));
+// console.log(updateScore('human'));
+// console.log(humanScore);
+// console.log(updateScore('computer'));
+// console.log(updateScore('computer'));
+// console.log(updateScore('computer'));
+// console.log(computerScore);
+
+
+const advanceRound = () => currentRoundNumber++;
+// console.log(advanceRound());
+// console.log(advanceRound());
 
