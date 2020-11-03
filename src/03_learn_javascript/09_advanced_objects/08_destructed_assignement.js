650d70c72a78eaf89fe93d32290fe51a7adf437f
Urs Albisser <u.albisser@gmail.com>
2020-11-03 14:12:44 +0100

    update

--- src/03_learn_javascript/09_advanced_objects/08_destructed_assignement.js
+++ src/03_learn_javascript/09_advanced_objects/08_destructed_assignement.js
@@ -18,8 +18,8 @@ console.log(residence); // Prints 'Transylvania'
 
 // However, we can also take advantage of a destructuring technique called destructured assignment to save ourselves some keystrokes. In destructured assignment we create a variable with the name of an object’s key that is wrapped in curly braces { } and assign to it the object. Take a look at the example below:
 
-const { residence } = vampire;
-console.log(residence); // Prints 'Transylvania'
+const { residence2 } = vampire;
+console.log(residence2); // Prints 'Transylvania'
 
 // Look back at the vampire object’s properties in the first code example. Then, in the example above, we declare a new variable residence that extracts the value of the residence property of vampire. When we log the value of residence to the console, 'Transylvania' is printed.
 //
