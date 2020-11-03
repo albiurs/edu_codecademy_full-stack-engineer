99fe4cb8e81dff754306203e18adc7bd5b3f7b4c
Urs Albisser <u.albisser@gmail.com>
2020-11-03 13:45:07 +0100

    update meal maker

--- src/03_learn_javascript/09_advanced_objects/project_meal_maker/meal_maker.js
+++ src/03_learn_javascript/09_advanced_objects/project_meal_maker/meal_maker.js
@@ -5,29 +5,38 @@
 //     In this project, you’ll use JavaScript to randomly create a three-course meal based on what is available on a menu. We’ll keep running it until we’re satisfied with the generated meal!
 
 
+/**
+ * menu
+ *
+ * @type {{_courses: {desserts: [], appetizers: [], mains: []}, desserts, readonly courses: {desserts: menu.desserts, appetizers: menu.appetizers, mains: menu.mains}, generateRandomMeal(): string, appetizers, getRandomDishFromCourse(*): *, mains, addDishToCourse(*, *=, *=): void}}
+ */
 const menu = {
+
+    // menu courses model, holding the menus in an array
     _courses: {
         appetizers: [],
         mains: [],
         desserts: []
     },
+
+    // getter & setter methods
     get appetizers() {
        return this._courses.appetizers;
     },
     set appetizers(appetizerIn) {
-       this._courses.appetizers = appetizerIn;
+       this._courses.appetizers.push(appetizerIn);
     },
     get mains() {
        return this._courses.mains;
     },
     set mains(mainsIn) {
-       this._courses.mains = mainsIn;
+       this._courses.mains.push(mainsIn);
     },
     get desserts() {
        return this._courses.desserts;
     },
     set desserts(dessertsIn) {
-       this._courses.desserts = dessertsIn;
+       this._courses.desserts.push(dessertsIn);
     },
     get courses() {
         return {
@@ -36,22 +45,27 @@ const menu = {
             desserts: this.desserts
         }
     },
+
+    // method adding new dishes to the _course model
     addDishToCourse(courseName, dishName, dishPrice) {
         const dish = {
             name: dishName,
             price: dishPrice
         };
-        this._courses[courseName].push(dish);
-        // also try using your setter method!
-        // if(courseName === 'appetizers') this.appetizers = dish;
-        // if(courseName === 'mains') this.mains = dish;
-        // if(courseName === 'desserts') this.desserts = dish;
+        if(courseName === 'appetizers') this.appetizers = dish;
+        if(courseName === 'mains') this.mains = dish;
+        if(courseName === 'desserts') this.desserts = dish;
     },
+
+    // randomly gets a dish from the _course model using a course name: 'appetizers', 'mains', 'desserts'
     getRandomDishFromCourse(courseName) {
         const dishes = this._courses[courseName];
         rand = Math.floor(Math.random() * dishes.length);
         return dishes[rand];
     },
+
+    // generates a full random meal.
+    // returns a string with the meal & price.
     generateRandomMeal() {
         const appetizer = this.getRandomDishFromCourse('appetizers');
         const main = this.getRandomDishFromCourse('mains');
@@ -82,4 +96,6 @@ console.log(menu.desserts);
 // get & log a full random menu
 const randomMeal = menu.generateRandomMeal();
 console.log('Random Meal is: ' + randomMeal);
+// returns something like:
+// Random Meal is: Your meal is Peanuts, Stake with Fried Rice, Cake. The price is $25.
 
