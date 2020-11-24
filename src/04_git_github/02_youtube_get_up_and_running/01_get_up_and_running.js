e38dadf54cb4566956c02d5cddf4ed3cf779913f
Urs Albisser <u.albisser@gmail.com>
2020-11-24 18:19:00 +0100

    added markdown intro video

---
+++ src/04_git_github/02_youtube_get_up_and_running/01_get_up_and_running.js
@@ -0,0 +1,7 @@
+// Get Up and Running
+// GitHub
+// https://www.youtube.com/playlist?list=PLg7s6cbtAD15G8lNyoaYDuKZSKyJrgwB-
+//
+// In this resource, you will learn about version control using Git and GitHub. This is helpful if you’d like to keep
+// track of changes to your code locally or remotely, share your work with other software developers and organize your
+// development process so that you can explore adding new features while maintaining an operational codebase.
---
+++ src/04_git_github/03_github_lab/02_github_lab.js
@@ -0,0 +1,7 @@
+// GitHub Lab
+// GitHub
+// https://lab.github.com/
+//
+// In this resource, you will learn how to effectively manage control flow with GitHub. This is helpful if you’d like
+// to practice comprehensive tutorials on how to master a key component of programming - the ability to share and
+// merge your work with teammates safely using one of the most popular repository hosting services.
