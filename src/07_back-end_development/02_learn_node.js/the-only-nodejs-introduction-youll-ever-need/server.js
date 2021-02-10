d3ce9e93eb5ebb46183b8054d295830be7739f52
Urs Albisser <u.albisser@gmail.com>
2021-02-10 12:00:34 +0100

    update

--- src/07_back-end_development/02_learn_node.js/the-only-nodejs-introduction-youll-ever-need/server.js
+++ src/07_back-end_development/02_learn_node.js/the-only-nodejs-introduction-youll-ever-need/server.js
@@ -65,37 +65,65 @@ The only NodeJs introduction you'll ever need.
 //
 // So when we do url.parse(request.url).pathname , it gives us the pathname or the url which is primarily what we use
 // to route requests. But things can get easier though.
-const http = require('http'),
-    url = require('url'),
-
-    makeServer = function (request,response){
-        let path = url.parse(request.url).pathname;
-        console.log(path);
-        if(path === '/'){
-            response.writeHead(200,{'Content-Type':'text/plain'});
-            response.write('Hello world');
-        }
-        else if(path === '/about'){
-            response.writeHead(200,{'Content-Type':'text/plain'});
-            response.write('About page');
-        }
-        else if(path === '/blog'){
-            response.writeHead(200,{'Content-Type':'text/plain'});
-            response.write('Blog page');
-        }
-        else{
-            response.writeHead(404,{'Content-Type':'text/plain'});
-            response.write('Error page');
-        }
-        response.end();
-    },
-    server = http.createServer(makeServer);
-server.listen(3000,()=>{
-    console.log('Node server created at port 3000');
-});
+// const http = require('http'),
+//     url = require('url'),
+//
+//     makeServer = function (request,response){
+//         let path = url.parse(request.url).pathname;
+//         console.log(path);
+//         if(path === '/'){
+//             response.writeHead(200,{'Content-Type':'text/plain'});
+//             response.write('Hello world');
+//         }
+//         else if(path === '/about'){
+//             response.writeHead(200,{'Content-Type':'text/plain'});
+//             response.write('About page');
+//         }
+//         else if(path === '/blog'){
+//             response.writeHead(200,{'Content-Type':'text/plain'});
+//             response.write('Blog page');
+//         }
+//         else{
+//             response.writeHead(404,{'Content-Type':'text/plain'});
+//             response.write('Error page');
+//         }
+//         response.end();
+//     },
+//     server = http.createServer(makeServer);
+// server.listen(3000,()=>{
+//     console.log('Node server created at port 3000');
+// });
 
 
 
 
 // Routing with Express
+// Whenever you see a .use() method in express, it’s called a "middleware". Middlewares are functions that do some http
+// heavy lifting for us. In the code, the middleware is an error handling one.
+//server.js
+const express = require('express'),
+    server = express();
 
+server.set('port', process.env.PORT || 3000);
+
+//Basic routes
+server.get('/', (request,response)=>{
+    response.send('Home page');
+});
+
+server.get('/about',(request,response)=>{
+    response.send('About page');
+});
+
+//Express error handling middleware
+
+server.use((request,response)=>{
+    response.type('text/plain');
+    response.status(505);
+    response.send('Error page');
+});
+
+//Binding to a port
+server.listen(3000, ()=>{
+    console.log('Express server started at port 3000');
+});
